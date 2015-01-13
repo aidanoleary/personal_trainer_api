c3e82fc4dcab0f9e84995bf647728b0cdf95af46
aidanoleary <aidanoleary92@gmail.com>
2015-01-13 18:02:04 +0000

    Initial commit - API now allows users to sign up, log in and sign out. It implements token based authentication.

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,30 @@
+class RegistrationsController < Devise::RegistrationsController
+  #skip_before_filter :verify_authenticity_token,
+  #                   :if => Proc.new { |c| c.request.format == 'application/json' }
+
+  respond_to :json
+
+  def create
+    @user = User.create(user_params)
+    if @user.save
+      render :status => 200,
+             :json => { :success => true,
+                        :info => "Registered",
+                        :data => { :user => @user.email,
+                                   :auth_token => @user.authentication_token } }
+    else
+      render :status => :unprocessable_entity,
+             :json => { :success => false,
+                        :info => resource.errors,
+                        :data => {} }
+    end
+
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
+
+end
