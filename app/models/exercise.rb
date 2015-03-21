4f65575c74e966ba44036f40520554633895037a
aidanoleary <aidanoleary92@gmail.com>
2015-03-21 15:32:39 +0000

    Changed the location that exercise images are uploaded to.

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -4,7 +4,10 @@ class Exercise < ActiveRecord::Base
   has_many :workouts, through: :workout_exercises
 
   # Paperclip image attachment
-  has_attached_file :e_image, :styles => {:medium => "300x300>", :thumb => "100x100>"}, :default_url => "/images/:style/missing.png"
+  has_attached_file :e_image, :styles => {:medium => "300x300>", :thumb => "100x100>"},
+                    :url => "/assets/exercises/:id/:style/:basename.:extension",
+                    :path => ":rails_root/public/assets/exercises/:id/:style/:basename.:extension",
+                    :default_url => "/assets/exercises/:style/missing.png"
   #validates_attachment_content_type :e_image, :content_type => /\Aimage\/.*\/z/
   validates_attachment :e_image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png"] }
 
